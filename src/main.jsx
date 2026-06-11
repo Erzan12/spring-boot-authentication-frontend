938cad6cc2fbbe354de75522988622ab7614867f
erzan12 <do.earljan@gmail.com>
2026-06-11 14:09:13 +0800

    fix: main.jsx added strict mode on react

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
+import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
 
 ReactDOM.createRoot(
     document.getElementById("root")
 ).render(
-    <AuthProvider>
-        <App />
-    </AuthProvider>
+    <React.StrictMode>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </React.StrictMode>
 );
